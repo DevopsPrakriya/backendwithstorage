ea8a37d58084b788b6e53ab7292b5bda6b0ac352
swapnil060791 <saxenaswapnil033@gmail.com>
2025-07-14 22:55:02 +0530

    asr

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "rg1" {
+    name     = "arsrg1"
+    location = "West Europe"
+  
+}
+
+resource "azurerm_resource_group" "rg2" {
+    name     = "arsrg2"
+    location = "West Europe"
+  
+}
