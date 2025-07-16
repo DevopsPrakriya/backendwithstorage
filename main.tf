964789deb967e8d8f302ed3ab6f340df1766a5ab
swapnil060791 <saxenaswapnil033@gmail.com>
2025-07-16 23:47:17 +0530

    stg8 added

--- main.tf
+++ main.tf
@@ -64,3 +64,10 @@ resource "azurerm_storage_account" "storage7" {
     account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_account" "storage8" {
+    name                     = "b35arsstorage8"
+    resource_group_name      = azurerm_resource_group.rg2.name
+    location                 = azurerm_resource_group.rg2.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
