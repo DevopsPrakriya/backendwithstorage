cfe91a9d27f3167892759708072288cce7ef986a
swapnil060791 <saxenaswapnil033@gmail.com>
2025-07-16 23:29:08 +0530

    v5 3 new storage

--- main.tf
+++ main.tf
@@ -41,3 +41,24 @@ resource "azurerm_storage_account" "storage4" {
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
+resource "azurerm_storage_account" "storage4" {
+    name                     = "b35arsstorage5"
+    resource_group_name      = azurerm_resource_group.rg2.name
+    location                 = azurerm_resource_group.rg2.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+resource "azurerm_storage_account" "storage4" {
+    name                     = "b35arsstorage6"
+    resource_group_name      = azurerm_resource_group.rg2.name
+    location                 = azurerm_resource_group.rg2.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+resource "azurerm_storage_account" "storage4" {
+    name                     = "b35arsstorage7"
+    resource_group_name      = azurerm_resource_group.rg2.name
+    location                 = azurerm_resource_group.rg2.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
