0c29be552772d4ea16047f22a3d0764beaaa4618
Amol AK (EXT-Nokia) <amol.ak.ext@nokia.com>
2025-07-17 01:41:37 +0530

    stg9 added

--- main.tf
+++ main.tf
@@ -71,3 +71,10 @@ resource "azurerm_storage_account" "storage8" {
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
+resource "azurerm_storage_account" "storage9" {
+    name                     = "b35arsstorage9"
+    resource_group_name      = azurerm_resource_group.rg2.name
+    location                 = azurerm_resource_group.rg2.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
