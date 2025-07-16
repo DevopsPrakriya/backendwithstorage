afbeffd28987b246cf332b3d8f9c75fc9625067f
swapnil060791 <saxenaswapnil033@gmail.com>
2025-07-16 18:15:08 +0530

    v2 ek naya storage

--- main.tf
+++ main.tf
@@ -41,3 +41,11 @@ resource "azurerm_storage_account" "storage4" {
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_account" "storage5" {
+    name                     = "b35arsstorage5"
+    resource_group_name      = azurerm_resource_group.rg2.name
+    location                 = azurerm_resource_group.rg2.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
